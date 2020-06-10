0ae95c60c8e7104d600e52452360d17972907b55
Bohdan <bohdan.shurlo@gmail.com>
2020-06-10 11:34:45 +0300

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,42 @@ const gulp = require('gulp'),
 
 
 gulp.task('scss', function() {
-  return gulp.src('dist/scss/style.scss')
+  return gulp.src('docs/scss/style.scss')
   .pipe(sass({outputStyle: 'expanded'}))
   .pipe(autoprefixer({
     cascade: false
   }))
-  .pipe(gulp.dest('dist/css'))
+  .pipe(gulp.dest('docs/css'))
   .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('html', function() {
-  return gulp.src('dist/*.html')
+  return gulp.src('docs/*.html')
   .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('js', function() {
-  return gulp.src('dist/js/*.js')
+  return gulp.src('docs/js/*.js')
   .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: "dist/"
+      baseDir: "docs/"
     }
   });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('dist/scss/**/*.scss', gulp.parallel('scss'))
-  gulp.watch('dist/*html', gulp.parallel('html'))
-  gulp.watch('dist/js/*js', gulp.parallel('js'))
+  gulp.watch('docs/scss/**/*.scss', gulp.parallel('scss'))
+  gulp.watch('docs/*html', gulp.parallel('html'))
+  gulp.watch('docs/js/*js', gulp.parallel('js'))
 });
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'));
 
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src('./docs/**/*')
   .pipe(ghPages());
 });
